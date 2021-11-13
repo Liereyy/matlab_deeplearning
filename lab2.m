23250985f5417e481c2c422b845de9c674f00462
Liereyy <xh643931341@mail.ustc.edu.cn>
2021-11-13 20:05:18 +0800

    2

--- lab2.m
+++ lab2.m
@@ -1,43 +1,50 @@
 warning('off');
+clear;
 
 P = [-2 -2 -2 -2 -2 -1 -1 -1 -1 -1  0  0 0 0 0  1  1 1 1 1  2  2 2 2 2;
      -2 -1  0  1  2 -2 -1  0  1  2 -2 -1 0 1 2 -2 -1 0 1 2 -2 -1 0 1 2];
 
 T = [-2 -2 -1 -1 0  -2 -1 -1  0  0 -1 -1 0 0 1 -1  0 0 1 1  0  0 1 1 2];
 
-for S1 = 7 : 1 : 20
-    [R, ~] = size(P);
-    [S2, Q] = size(T);
+S1 = 10;
+[R, ~] = size(P);
+[S2, Q] = size(T);
 
-    net = newff(minmax(P), [S1,1], {'tansig', 'purelin'}, 'trainlm'); 
+net = newff(minmax(P), [S1,1], {'tansig', 'purelin'}, 'traingd'); 
 
-    net.performFcn = 'sse';
-    net.trainParam.epochs = 50000;
-    net.trainParam.lr = 0.01;
-    net.trainParam.goal = 1e-5;
+% 初始化
+net.IW{1, 1} = rands(S1, R);
+net.b{1} = rands(S1, 1);
+net.LW{2, 1} = rands(S2, S1);
+net.b{2} = rands(S2, 1);
 
-    [net,tr]= train(net, P, T); 
-    Y = sim(net ,P);
+net.trainParam.epochs = 25000;
+net.trainParam.lr = 0.08;
+net.trainParam.goal = 1e-3;
 
-    % 样本插值测试
-    P1 = zeros();
-    T1 = zeros();
-    step = 0.5;
-    e1 = -2:step:2;
-    ec1 = -2:step:2;
-    [~, S] = size(e1);
-    for i = 1:S
-        for j = 1:S
-            col = (i-1) * S + j;
-            P1(1, col) = e1(i);
-            P1(2, col) = ec1(j);
-            T1(col) = floor((e1(i) + ec1(j)) / 2);
-        end
+[net,tr]= train(net, P, T, nnMATLAB); 
+Y = sim(net ,P);
+
+sse_error = perform(net, T, Y)  ;
+fprintf('SSE = %f\n', sse_error);
+
+% 样本插值测试
+P1 = zeros();
+T1 = zeros();
+step = 0.5;
+e1 = -2:step:2;
+ec1 = -2:step:2;
+[~, S] = size(e1);
+for i = 1:S
+    for j = 1:S
+        col = (i-1) * S + j;
+        P1(1, col) = e1(i);
+        P1(2, col) = ec1(j);
+        T1(col) = floor((e1(i) + ec1(j)) / 2);
     end
-    A = sim(net, P1);
-    % A = floor(A);
-    %Res = cat(1, A, T1)
-    [~, C] = size(A);
-    ave_error = sse(T1-A) / C;
-    fprintf('%d %f\n', S1, ave_error);
 end
+A = sim(net, P1);
+L = cat(1, A, T1)
+
+sse_error = perform(net, T1, A);
+fprintf('SSE = %f\n', sse_error);
--- untitled.m
+++ untitled.m
@@ -1,3 +1,6 @@
+warning('off');
+clear;
+
 P = [-2 -2 -2 -2 -2 -1 -1 -1 -1 -1  0  0 0 0 0  1  1 1 1 1  2  2 2 2 2;
      -2 -1  0  1  2 -2 -1  0  1  2 -2 -1 0 1 2 -2 -1 0 1 2 -2 -1 0 1 2];
 
@@ -7,32 +10,32 @@ S1 = 10;
 [R, ~] = size(P);
 [S2, Q] = size(T);
 
-%net = newff(minmax(P), [S1,1], {'tansig', 'purelin'}, 'trainlm'); 
-net = newff(P, T, S1, {'tansig', 'purelin'}, 'trainlm');
-net.performFcn = 'sse';
-net.trainParam.epochs = 50000;
-net.trainParam.lr = 0.01;
-net.trainParam.goal = 1e-5;
+net = newff(minmax(P), [S1,1], {'tansig', 'purelin'}, 'trainlm'); 
 
-[net,tr]= train(net, P, T); 
+% 初始化
+net.IW{1, 1} = rands(S1, R);
+net.b{1} = rands(S1, 1);
+net.LW{2, 1} = rands(S2, S1);
+net.b{2} = rands(S2, 1);
+
+% 打印初始权重
+net.IW{1, 1}
+net.b{1}
+net.LW{2, 1}
+net.b{2}
+
+net.trainParam.epochs = 25000;
+net.trainParam.lr = 0.08;
+net.trainParam.goal = 1e-3;
+net.trainParam.mc = 0.9;
+
+[net,tr]= train(net, P, T, nnMATLAB); 
 Y = sim(net ,P);
 
-% 样本插值测试
-P1 = zeros();
-T1 = zeros();
-step = 1;
-e1 = -2:step:2;
-ec1 = -2:step:2;
-[~, S] = size(e1);
-for i = 1:S
-    for j = 1:S
-        col = (i-1) * S + j;
-        P1(1, col) = e1(i);
-        P1(2, col) = ec1(j);
-        T1(col) = floor((e1(i) + ec1(j)) / 2);
-    end
-end
-A = sim(net, P1);
-Res = cat(1, A, T1);
-[~, C] = size(A);
-ave_error = sse(T1-A) / C
+sse_error = perform(net, T, Y);
+fprintf('SSE = %f\n', sse_error);
+% 打印训练完毕后的权重
+net.IW{1, 1}
+net.b{1}
+net.LW{2, 1}
+net.b{2}
